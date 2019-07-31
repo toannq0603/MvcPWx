52f0c654fab07690befad3d6a28c177920c11c30
toannq06031999 <toannq06031999@gmail.com>
2019-07-31 07:44:10 +0700

    Add project files.

---
+++ MvcPWx/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcPWx.MvcApplication" Language="C#" %>
---
+++ MvcPWx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPWx.Startup))]
+namespace MvcPWx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
